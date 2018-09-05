6817f32301f444b934f2b1565f4bf5e4d69482c7
Kevin Phillips <kphillips86@gmail.com>
2018-09-04 22:01:40 -0500

    adding graph for each dependency

--- dependency-data/dependency-data.js
+++ dependency-data/dependency-data.js
@@ -1,6 +1,78 @@
 import { Component, DefineMap } from "can";
 import recast from "recast";
+import vis from "vis";
 import getDependencies from "./get-dependencies";
+import "./dependency-data.less";
+
+Component.extend({
+	tag: "dependency-graph",
+
+	view: `
+		<h3>dependencies for "{{key}}":</h3>
+		<div class="graph"></div>
+	`,
+
+	ViewModel: {
+		connectedCallback(el) {
+			const key = this.key
+			const dependencies = this.dependencyData;
+
+			// console.log("dependencies", dependencies);
+
+			let nodeId = 1;
+			const nodeToIdMap = {
+				[key]: nodeId
+			};
+
+			const nodes = [
+			{ id: 1, label: key }
+			];
+			const edges = [];
+
+			const addNodesAndEdges = (key) => {
+				const deps = dependencies[key];
+
+				if (deps) {
+					deps.forEach((dep) => {
+						// add node for this dependency if it doesn't exist
+						if (!nodeToIdMap[dep]) {
+							nodeToIdMap[dep] = ++nodeId;
+							nodes.push({ id: nodeId, label: dep });
+						}
+
+						// add edge for this dependency
+						edges.push({
+							from: nodeToIdMap[dep],
+							to: nodeToIdMap[key],
+							arrows: "to"
+						});
+
+						// add nodes and edges for dependencies of this dependency
+						addNodesAndEdges(dep);
+					});
+				}
+			};
+
+			addNodesAndEdges(key);
+
+			const options = {
+				layout: {
+					hierarchical: {
+						direction: "RL"
+					}
+				}
+			};
+
+			new vis.Network(el.querySelector(".graph"), {
+				nodes: new vis.DataSet(nodes),
+				edges: new vis.DataSet(edges)
+			}, options);
+
+		},
+		key: "string",
+		dependencyData: "any"
+	}
+});
 
 Component.extend({
 	tag: "dependency-data",
@@ -9,34 +81,31 @@ Component.extend({
 		<h2 class="center">Dependencies</h2>
 
 		<div>
-			{{# each(vmDependencies, key=key deps=value) }}
-				{{ key }}
-				<ul>
-						{{# each(deps, dep=value) }}
-							<li>{{ dep }}</li>
-						{{/ each }}
-				</ul>
+			{{# each(dependencies, key=key }}
+				<dependency-graph key:from="key" dependencyData:from="scope.vm.dependencies"></dependency-graph>
 			{{/ each }}
 		</div>
 	`,
 
 	ViewModel: {
-		vmProps: "any",
+		propDefinitions: "any",
 
 		VM: {
 			value({ listenTo, resolve }) {
 				const update = () => {
 					try {
-						const c = new Function("C", `return C.extend(${this.vmProps});`);
+						const c = new Function("C", `return C.extend(${this.propDefinitions});`);
 						const C = c(DefineMap);
-						resolve (C);
+						resolve(C);
 					} catch(e) {
 						// if creating constructor throws, fail silently
 						// the user probably isn't done typing
+						// ie user is in the middle of typing a property name
+						// `fo`
 						// console.info("Creating VM failed", e);
 					}
 				};
-				listenTo("vmProps", update);
+				listenTo("propDefinitions", update);
 				update();
 			}
 		},
@@ -45,8 +114,24 @@ Component.extend({
 			return new this.VM();
 		},
 
-		get vmDependencies() {
-			return getDependencies(this.vm);
+		dependencies: {
+			value({ listenTo, resolve }) {
+				const update = (vm) => {
+					try {
+						const deps = getDependencies(this.vm);
+						resolve(deps);
+					} catch(e) {
+						// if getting dependencies throws, fail silently
+						// the user probably isn't done typing
+						// ie user is in the middle of typing a getter
+						// `get foo() { ret }`
+						// console.info("getting dependencies failed", e);
+					}
+				};
+
+				listenTo("vm", update);
+				update(this.vm);
+			}
 		}
 	}
 });
